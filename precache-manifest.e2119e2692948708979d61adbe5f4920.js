c0e4a02f83c0be39c0b6508ca85a06c65963dc22
sohel2178 <sohel.ahmed2178@gmail.com>
2020-06-16 02:24:16 +0600

    Updates

---
+++ precache-manifest.e2119e2692948708979d61adbe5f4920.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "663531e175625c1508fd72c66fb697ca",
+    "url": "/privacy-policies/index.html"
+  },
+  {
+    "revision": "d74841bdb1f0ecdfd546",
+    "url": "/privacy-policies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52fd713940813888b4b2",
+    "url": "/privacy-policies/static/js/2.de79498f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/privacy-policies/static/js/2.de79498f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74841bdb1f0ecdfd546",
+    "url": "/privacy-policies/static/js/main.f7148c2f.chunk.js"
+  },
+  {
+    "revision": "e4b9c00b5d22a09175dc",
+    "url": "/privacy-policies/static/js/runtime-main.c104fd50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/privacy-policies/static/media/logo.5d5d9eef.svg"
+  }
+]);
